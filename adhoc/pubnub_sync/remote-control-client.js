58c21105bc66684d7ec10ee8ae2ffdba9785ba29
db4rr <dbarr@dolby.com>
2017-07-31 16:58:29 +0200

    remote control without ssl

--- adhoc/pubnub_sync/remote-control-client.js
+++ adhoc/pubnub_sync/remote-control-client.js
@@ -3,7 +3,7 @@
 var pubnub = new PubNub({
     publishKey: 'pub-c-4b844d24-d357-4c0a-aeaa-b82e30decead',
     subscribeKey: 'sub-c-56c04294-b696-11e6-b37b-02ee2ddab7fe',
-    ssl: true
+    ssl: false
 });
 
 pubnub.addListener({
