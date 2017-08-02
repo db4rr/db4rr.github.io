9ddeb515dd19af87de74c38d829bdf54675ee092
db4rr <dbarr@dolby.com>
2017-08-02 13:02:30 +0200

    favicon + autobahn

--- adhoc/pubnub_sync/configuration.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'publishKey' => 'pub-c-4b844d24-d357-4c0a-aeaa-b82e30decead',
-    'subscribeKey' => 'sub-c-56c04294-b696-11e6-b37b-02ee2ddab7fe',
-    'inputChannel' => 'input',
-    'allowedUsernames' => ['DBF_telegram'],
-];
